e6935e05c077a8586a01f06b4e532b9800981169
Vecordis <32594043+Effanuel@users.noreply.github.com>
2019-02-12 18:56:30 +0200

    Quick sort algoritmas

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+array = np.random.randint(1, 100, size=10000)
+
+
+def quicksort(x):
+      if len(x) < 2:
+          return x
+      else:
+          pivot = x[0]
+          less = [i for i in x[1:] if i <= pivot]
+          greater = [i for i in x[1:] if i > pivot]
+          return quicksort(less) + [pivot] + quicksort(greater)
+        
+quicksort(array)
